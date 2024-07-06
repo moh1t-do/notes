4090a9ca93caa4389a6a80d49766531324c93f0d
Mohit <mohitd2288@gmail.com>
2024-07-06 12:10:25 +0530

    update ignore, update template

--- template/code.cpp
+++ template/code.cpp
@@ -1,21 +1,24 @@
 #include <bits/stdc++.h>
-#define fast                        \
-  ios_base::sync_with_stdio(false); \
-  cin.tie(NULL);
+#define fast                          \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
 using namespace std;
+
+#define int long long
 
 void solve()
 {
-  // /INPUT
+    
+    // /INPUT
 }
 
-int main()
+int32_t main()
 {
-  fast;
-  int t{1};
-  cin >> t;
-  while (t--)
-    solve();
+    fast;
+    int t{1};
+    cin >> t;
+    while (t--)
+        solve();
 
-  return 0;
+    return 0;
 }
